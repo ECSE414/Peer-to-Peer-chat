82fbb6a09347baeeab4b284755abeb78503444f4
Stephen Carter <stephen.carter@mail.mcgill.ca>
2015-11-03 17:41:14 -0500

    first commit

---
+++ clientServerClient.py
@@ -0,0 +1,40 @@
+﻿import socket
+import time
+from random import randint
+
+HOST = ''
+PORT = 5000
+
+def main():
+    test_server = Server();
+    test_server.start()
+
+
+#class Client():
+#    def __init__(self):
+#        self.host = None
+#        self.sock = None
+#        self.active = True;
+    
+    
+class Server():
+    def __init__(self):
+        self.addr = None
+        self.Active = True
+
+    def start(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind((HOST, PORT))
+        while 1:
+            data, self.addr = s.recvfrom(1024)
+            
+            if not data:
+                break;
+
+            answer = 'OK...' + data
+            s.sendto(answer, addr)
+            print("[" + addr[0] + ":" + str(addr[1]) + "] :: " + data)
+    def kill(self):
+        self.Active = False
+
+
