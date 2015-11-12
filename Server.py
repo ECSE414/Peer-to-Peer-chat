f999d993b343919e29334402cfd82e0f9a744eee
Stephen Carter <stephen.carter@mail.mcgill.ca>
2015-11-11 21:32:22 -0500

    seperate files and some UX design added

---
+++ Server.py
@@ -0,0 +1,61 @@
+import socket   #socket for Peer-to-Peer and Client-Server comms.
+import time     
+import sys      #sys calls
+import select   #import to poll keyboard for press
+import datetime
+
+SERVER_IP = '159.203.31.96'  #server IP
+SERVER_PORT = 6000         #server port
+
+def main():
+    print socket.gethostbyname(socket.gethostname())
+    server = Server()
+    server.start()
+    
+class Server():
+    def __init__(self):
+        self.addr = None
+        self.active = True
+        self.for_table = { '0' : SERVER_IP + ":" + str(SERVER_PORT) };
+        
+    def start(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind((SERVER_IP, SERVER_PORT))
+        while 1:
+            k = 0
+            out = s.recvfrom(1024)
+            data = out[0]
+            self.addr = out[1]
+            print self.addr
+            if not data:
+                break;
+            result = data.split(':')
+            if data == result[1]:
+                answer = self.for_table[data]
+            else:
+                for i in self.for_table.keys():
+                    if i = result[0]:
+                        answer = 'That numerical ID is taken, please restart the messenger and try again'
+                        k = 1
+                        break
+                    else:
+                        answer = 'IP...' + result[1] + ' port...' + result[2]
+                if k == 0:
+                    self.for_table[result[0]] = result[1] + ":" + result[2]
+                
+                
+            send_to = self.for_table[result[0]].split(':')
+            print send_to[0]
+            print send_to[1]
+            #s.sendto(answer, (send_to[0], int(send_to[1])))
+            s.sendto(answer, self.addr)
+            #print int(result[1])
+            
+            print "[" + self.addr[0] + ":" + str(self.addr[1]) + "] :: " + data
+            print self.for_table
+    def kill(self):
+        self.active = False
+        s.close()
+
+if __name__ == "__main__":
+    main()
---
+++ clientClient.py
@@ -0,0 +1,97 @@
+﻿import socket   #socket for Peer-to-Peer and Client-Server comms.
+import time     
+import sys      #sys calls
+import select   #import to poll keyboard for press
+import datetime
+
+SERVER_IP = '159.203.31.96'  #server IP
+SERVER_PORT = 6000         #server port
+
+def main():
+    print socket.gethostbyname(socket.gethostname())
+    num = raw_input('Please enter your numerical id: ')
+    client = Client()
+    client.ID = num
+    print client.ID
+    #sel_port = raw_input('Please enter a port number greater than 2000: ')
+
+    client.host = socket.gethostbyname(socket.gethostname())
+    client.port = 8000
+    client.setupServerConn()
+    to = raw_input('Who would you like to contact?')
+    conn = client.requestBuddy(to)
+    client.setupChat(conn[0], int(conn[1]))
+
+class Client():
+    def __init__(self):
+        self.host = None
+        self.port = None
+        self.ID = None
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.active = True
+
+    def getLine(self):
+        i,o,e = select.select([sys.stdin],[],[],0.0001)
+        for s in i:
+            if s == sys.stdin:
+                input = sys.stdin.readline()
+                return input
+        return False
+    def setupChat(self, dest, dest_port):
+        self.kill()
+        self.active = True
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.bind((self.host, self.port))
+        self.s.setblocking(False)
+
+        while 1:
+
+            try:
+                out = self.s.recvfrom(1024)
+                data = out[0]
+                addr = out[1]
+                if data:
+                    print "[" + dest + ":" + str(dest_port) + "] :: " + data
+            except:
+                pass
+            
+            message = self.getLine()
+            if (message != False):
+                self.s.sendto(message, (dest, dest_port))
+       
+    def setupServerConn(self):
+        print self.host
+        print self.port
+        self.s.bind((self.host, self.port))
+        msg = str(self.ID) + ':' + self.host + ':' + str(self.port)
+        print msg
+        self.s.sendto(msg,(SERVER_IP, SERVER_PORT))
+        
+        out = self.s.recvfrom(1024)
+        data = out[0]
+        addr = out[1]
+
+        if data == 'That numerical ID is taken, please restart the messenger and try again':
+            print data + ' ... exiting'
+            exit(0)
+        print 'Your info: ' + data
+
+    def requestBuddy(self, who):
+       msg = str(self.ID) + ':' + str(who)
+
+       self.s.sendto(msg, (SERVER_IP, SERVER_PORT))
+
+       out = self.s.recvfrom(1024)
+
+       data = out[0]
+       addr = out[1]
+
+       print 'requested information: ' + data
+       result = data.split(':', 1)
+       return result
+    def kill(self):
+        self.active = False
+        self.s.close()
+
+if __name__ == "__main__":
+    main()
