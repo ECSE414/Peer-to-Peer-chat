cf299f874fd6726d94b233ca31615502e7634dc9
Stephen Carter <stephen.carter@mail.mcgill.ca>
2015-12-04 12:39:02 -0500

    implementaion changes

--- Server.py
+++ Server.py
@@ -13,7 +13,7 @@ RESV2 = '-2'
 RESV3 = '-3'
 RESV4 = '-4'
 def main():
-    print socket.gethostbyname(socket.gethostname())
+    #print socket.gethostbyname(socket.gethostname())
     #start server
     server = Server()
     server.start()
@@ -57,6 +57,7 @@ class Server():
                     answer = self.avail[result[1]]
                 else:
                     answer = self.for_table[result[1]]
+                send = True
             #if RESV code 1 was sent...exit and delete entries
             elif result[1] == RESV1:
                 del self.for_table[result[0]]
@@ -69,13 +70,14 @@ class Server():
             #if RESV code 3 was sent, return available users
             elif result[1] == RESV3:
                 answer = str(self.avail.keys())
-            #if RESV code 4 was sent, return all users
+            #if RESV code 4 was sent, return all online
             elif result[1] == RESV4:
                 answer = str(self.for_table.keys())
-            #else means its a new entry to the fortable, so update or reject if ID is taken
+            #else means its a new entry to the forwarding table, so update or reject if ID is taken
             else:
                 answer = 'IP...' + result[1] + ' port...' + result[2]
                 for i in self.for_table:
+                    #if ID is taken return invalid seq
                     if i == result[0]:
                         answer = NO_NAME
                         add = 1
--- clientClient.py
+++ clientClient.py
@@ -3,33 +3,32 @@ import time
 import sys      #sys calls
 import select   #import to poll keyboard for press
 import datetime
-from urllib2 import urlopen
+from urllib2 import urlopen #used to get your IP
 
 SERVER_IP = '159.203.31.96'  #server IP
 SERVER_PORT = 6000          #server port
+
 check = 0                   #valid ID check
 printed = False             #something to print check
 
-client = None
+client = None               #globally accessible client
 NO_NAME = '%-1,'            #resevered character sequences
-RESV1 = '-1'
+RESV1 = '-1'                #Exit
 RESV2 = '-2'
 RESV3 = '-3'
 RESV4 = '-4'
 def main():
     global client
     global printed
-    #print socket.gethostbyname(socket.gethostname())
-    my_ip = urlopen('http://ip.42.pl/raw').read()
-    #print my_ip
-    client = Client()
-    while check == 0:
-        ret = enter_ID(my_ip)  #get users input
-        if ret == False:
+    my_ip = urlopen('http://ip.42.pl/raw').read()   #Get IP
+    client = Client()                               #created client
+    while check == 0:                               #while ID is not entered
+        ret = enter_ID(my_ip)                       #get users ID
+        if ret == False:                            #if invalid ID, try again
             continue
     while 1:
         try:
-            if not printed:
+            if not printed:                         #print menu if valid
                 print "Please enter a command: (type /help for help)"
                 printed = True
             ret = to_recv()       #check to see if data is to be received
@@ -41,46 +40,59 @@ def main():
             print()
             pass
 
+#!
+# Function used to get user ID from client and update corresponding server information
+#
 def enter_ID(my_ip):
     global client
     try:
+        #get users ID
         num = raw_input('Please enter your id: ')
-        if num == '-1' or num == '-2' or num == '-3' or num == '-4' or num == NO_NAME:
+        if num == RESV1 or num == RESV2 or num == RESV3 or num == RESV4 or num == NO_NAME:
             print "Invalid ID, try again"
             return False
+        #update client ID
         client.ID = num
-        #print client.ID
+        #update IP and port number
         client.host = my_ip
         client.port = 8000
+        #bind the socket to this port and IP
         client.s.bind((client.host, client.port))
+        #setup and send information to server
         client.setupServerConn()
 
     except KeyboardInterrupt:
         pass
-
+#!
+#Function to recv a request that interrupts the menu
+#
 def to_recv():
     global client
     global printed
     try:
+        #make sure that waiting to receive data is not blocking
         client.s.setblocking(False)
+        #bytes to recv
         out = client.s.recvfrom(1024)
+        #retreive data and senders information
         data = out[0]
         addr = out[1]
-        #print data
-        received = data.split(':')
+        #if data recveived is valid then ask if user wants to accept connection
         if data:
             answer = raw_input("Request from " + str(data)+ " do you want to accept? (y/n)")
-            #print answer
             conn = client.requestBuddy(data)
             if answer == "y":
-                #print "answer was good"
                 print "chat started"
+                #Send accept message to user who sent request
                 client.s.sendto("Chat request accepted", (conn[0], int(conn[1])))
+                #setup the chat
                 client.setupChat(conn[0], int(conn[1]))
                 printed = False
             else:
+                #if user does not wish to accept. connection is denied.
                 client.s.sendto("connection denied: returning to main menu", (conn[0], int(conn[1])))
-                client.s.sendto(str(client.ID) + ':-2', (SERVER_IP, SERVER_PORT))
+                #add user back to avail list on server.
+                client.s.sendto(str(client.ID) + ':' + RESV2, (SERVER_IP, SERVER_PORT))
                 printed = False
     except:
         client.s.setblocking(True)
@@ -89,45 +101,64 @@ def to_recv():
 def command_ready():
     global client
     global printed
+    #grab line when avail
     command = client.getLine()
+    #make sure line read is valid
     if (command != False):
+        #if user requests help, display menu  options
         if command == "/help\n":
             print "| req\t::\t'Request a Buddy'\t|\n| avail\t::\t'See available users'\t|\n| all\t::\t'See all users'\t\t|\n| exit\t::\t'Exit the application'\t|"
             printed = False
+        #if user wants to request a buddy
         elif command == "req\n":
-            client.s.sendto(str(client.ID) + ':-3', (SERVER_IP, SERVER_PORT))
-            loop = 1
-            while loop == 1:
-                out = client.s.recvfrom(1024)
-                data = out[0]
-                addr = out[1]
-                print data
-                loop = 0
+            #request list of available users
+            client.s.sendto(str(client.ID) + ":" + RESV3, (SERVER_IP, SERVER_PORT))
+            client.s.setblocking(True)
+            #wait to recv data from server
+            out = client.s.recvfrom(1024)
+            data = out[0]
+            addr = out[1]
+            #print list of available users
+            print data
+            listed = re.split('[|\'|,')
+            #ask user who they want to request
             print 'Who would you like to contact?'
             to = False
+            #keep trying to recv answer
             while not to:
                 to = client.getLine()
             check = to.split('\n')
+            #make sure that user did not request theirself
+            for i in listed:
+                if listed == check[0]
+                printed = False
+                return False
             if check[0] == client.ID:
                 printed = False
                 return False
+            #request information of user
             conn = client.requestBuddy(to)
+            #if request is not valid, return back to menu
             if conn == NO_NAME:
                 printed = False
                 return False
+            #send my ID to client who I wish to contact
             client.s.sendto(client.ID, (conn[0], int(conn[1])))
             client.s.setblocking(True)
+            #block and wait for recv
             data = client.s.recvfrom(1024)
             print(data[0])
+            #if connection was not accepted add back to avail list and return to menu
             if data[0] == "connection denied: returning to main menu":
                 dest = None
-                client.s.sendto(str(client.ID) + ':-2', (SERVER_IP, SERVER_PORT))
+                client.s.sendto(str(client.ID) + ':' +  RESV2, (SERVER_IP, SERVER_PORT))
                 printed = False
                 return False
+            #setup the chat
             client.setupChat(conn[0], int(conn[1]))
             printed = False
         elif command == "avail\n":
-            client.s.sendto(str(client.ID) + ':-3', (SERVER_IP, SERVER_PORT))
+            client.s.sendto(str(client.ID) + ':' + RESV3, (SERVER_IP, SERVER_PORT))
             loop = 1
             while loop == 1:
                 out = client.s.recvfrom(1024)
@@ -137,17 +168,17 @@ def command_ready():
                 loop = 0
             printed = False
         elif command == "all\n":
-            client.s.sendto(str(client.ID) + ':-4', (SERVER_IP, SERVER_PORT))
+            client.s.sendto(str(client.ID) + ':' + RESV4, (SERVER_IP, SERVER_PORT))
             loop = 1
             while loop == 1:
                 out = client.s.recvfrom(1024)
                 data = out[0]
                 addr = out[1]
-                #print data
+                print data
                 loop = 0
             printed = False
         elif command == "exit\n":
-            client.s.sendto(str(client.ID) + ":-1", (SERVER_IP,SERVER_PORT))
+            client.s.sendto(str(client.ID) + ':' + RESV1, (SERVER_IP,SERVER_PORT))
             printed = False
             client.kill()
             exit(0)
@@ -171,14 +202,7 @@ class Client():
                 return input
         return False
     def setupChat(self, dest, dest_port):
-        #self.kill()
-        self.active = True
-        #self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        #self.s.bind((self.host, self.port))
         self.s.setblocking(False)
-        #print dest
-        #print dest_port
-        chat = True
         while 1:
             try:
                 try:
@@ -189,7 +213,7 @@ class Client():
                         print "[" + dest + ":" + str(dest_port) + "] :: " + data
                         if data == "Buddy disconnected: returning to main menu":
                             dest = None
-                            self.s.sendto(str(self.ID) + ':-2', (SERVER_IP, SERVER_PORT))
+                            self.s.sendto(str(self.ID) + ':' + RESV2, (SERVER_IP, SERVER_PORT))
                             return
                 except:
                     pass
@@ -200,17 +224,14 @@ class Client():
             except KeyboardInterrupt:
                 if dest != None:
                     self.s.sendto("Buddy disconnected: returning to main menu", (dest, dest_port))
-                self.s.sendto(str(self.ID) + ':-2', (SERVER_IP, SERVER_PORT))
+                self.s.sendto(str(self.ID) + ':' + RESV2, (SERVER_IP, SERVER_PORT))
                 print()
                 return
 
     def setupServerConn(self):
         global check
-        #print self.host
-        #print self.port
-        #self.s.bind((self.host, self.port))
+
         msg = str(self.ID) + ':' + self.host + ':' + str(self.port)
-        #print msg
         self.s.sendto(msg,(SERVER_IP, SERVER_PORT))
 
         out = self.s.recvfrom(1024)
@@ -237,9 +258,11 @@ class Client():
        data = out[0]
        addr = out[1]
 
-       print 'requested information: ' + data
+
        if data == NO_NAME:
            return data
+
+       print 'requested information: ' + data
        result = data.split(':', 1)
        #print result
        return result
